32355c9bc37e8cf623cf3b95b89f476b785d2d48
Alison Schonauer <115823577+aschonauer@users.noreply.github.com>
2024-03-25 23:18:02 -0700

    Add files via upload

---
+++ Labs/Lab6/main.py
@@ -0,0 +1,43 @@
+
+def countInDegrees(array_list):
+    inDegrees = {}
+    for pair in array_list:
+        inDegrees[pair[0]] = 0
+        inDegrees[pair[1]] = 0
+    for pair in array_list:
+        inDegrees[pair[0]] += 1
+    return inDegrees
+
+
+def canFinish(numCourses, prerequisites):
+    # topological sort
+    queue = []
+    sorted = []
+    in_degrees = countInDegrees(prerequisites)
+    for key in in_degrees:
+        if key < 0 or key >= numCourses:
+            return False
+        if in_degrees[key] == 0:
+            queue.append(key)
+    while queue:
+        node_key = queue.pop(0)
+        sorted.append(node_key)
+        for pair in prerequisites:
+            if pair[1] == node_key:
+                in_degrees[pair[0]] -= 1
+                if in_degrees[pair[0]] == 0:
+                    queue.append(pair[0])
+    if len(sorted) != len(in_degrees):
+        return False
+    return True
+
+
+courses = 4
+prereq = [[1, 2], [2, 0], [3, 0], [3, 1]]
+print(canFinish(courses, prereq))
+print("Expected: True")
+
+courses = 5
+prereq = [[1, 2], [2, 0], [3, 0], [3, 1], [0, 1]]
+print(canFinish(courses, prereq))
+print("Expected: False")
